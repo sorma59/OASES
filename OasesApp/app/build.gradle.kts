7ea111e7ccee1623e9884b63e87dda201db7bf67
fiaxdev <fiaxcoding@gmail.com>
2025-10-03 14:08:37 +0200

    Bump version code to 2

--- OasesApp/app/build.gradle.kts
+++ OasesApp/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.unimib.oases"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
