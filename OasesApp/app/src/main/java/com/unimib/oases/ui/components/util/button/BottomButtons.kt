1b343ee7abcbb80c2a8ae08583ead27a32c38892
fiaxdev <fiaxcoding@gmail.com>
2026-01-12 00:28:25 +0100

    UI: Add responsive sizing to `BottomButtons`
    This commit introduces responsive sizing for the buttons in the `BottomButtons` component by utilizing the `WindowWidthSizeClass`.
    
    The buttons now adjust their size based on the available screen width:
    - **Compact:** Default size.
    - **Medium:** Buttons are set to a size of `100.dp`.
    - **Expanded:** Buttons are set to a size of `160.dp`.
    
    This change ensures that the UI components scale appropriately across different device form factors. The `LocalWindowSizeClass` is used to determine the current `widthSizeClass`.

--- OasesApp/app/src/main/java/com/unimib/oases/ui/components/util/button/BottomButtons.kt
+++ OasesApp/app/src/main/java/com/unimib/oases/ui/components/util/button/BottomButtons.kt
@@ -5,13 +5,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.unimib.oases.util.LocalWindowSizeClass
 
 @Composable
 fun BottomButtons(
@@ -21,11 +24,21 @@ fun BottomButtons(
     cancelButtonText: String = "Cancel",
     confirmButtonText: String = "Save"
 ) {
+    val windowSize = LocalWindowSizeClass.current.widthSizeClass
+
+    val size = when(windowSize){
+        WindowWidthSizeClass.Compact -> null
+        WindowWidthSizeClass.Medium -> 100.dp
+        WindowWidthSizeClass.Expanded -> 160.dp
+        else -> null
+    }
+
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
         modifier = modifier.fillMaxWidth().padding(24.dp)
     ) {
         OutlinedButton(
+            modifier = size?.let { Modifier.size(it) } ?: Modifier,
             onClick = {
                 onCancel()
             }
@@ -34,6 +47,7 @@ fun BottomButtons(
         }
         Spacer(modifier = Modifier.width(16.dp))
         Button(
+            modifier = size?.let { Modifier.size(it) } ?: Modifier,
             onClick = {
                 onConfirm()
             }
