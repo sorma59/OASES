7af13c975fe816602a0ae81d3097db8bac3e59d7
fiaxdev <fiaxcoding@gmail.com>
2026-01-25 15:01:45 +0100

    Chore: Bump version code to 5
    This commit increments the application's `versionCode` from 4 to 5.

--- OasesApp/app/build.gradle.kts
+++ OasesApp/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.unimib.oases"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
