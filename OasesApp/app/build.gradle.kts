5900efcc122f85898c1498900f4a2f8ba7fc5028
fiaxdev <fiaxcoding@gmail.com>
2026-02-18 01:25:27 +0100

    Chore: Bump version code to 6

--- OasesApp/app/build.gradle.kts
+++ OasesApp/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.unimib.oases"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
